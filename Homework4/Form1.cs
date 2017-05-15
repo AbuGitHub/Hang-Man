93a4af013610dd702f7f14a16f199cf2a82fe4a6
Abuzar Noorali <abuzar.noorali@hotmail.com>
2017-05-15 13:41:00 -0500

    First commit

--- Homework4/Form1.cs
+++ Homework4/Form1.cs
@@ -7,14 +7,125 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using System.Reflection;
 
 namespace Homework4
 {
     public partial class Form1 : Form
     {
+        Random rnd = new Random();                                                          
+        int hangManAnswer;
+        int counter = 0;
+        string userInput;
+        int userInputUsable;
+
         public Form1()
         {
             InitializeComponent();
+            hangManAnswer = rnd.Next(1, 100);                                                
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        { 
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            userInput = textBox1.Text;
+            int.TryParse(userInput, out userInputUsable);
+            counter++;
+
+            switch(counter)
+            {
+                case 1:
+                    {
+                        pictureBox1.Image = Properties.Resources.hangman01;
+                        break;
+                    }
+
+                case 2:
+                    {
+                        pictureBox1.Image = Properties.Resources.hangman02;
+                        break;
+                    }
+
+                case 3:
+                    {
+                        pictureBox1.Image = Properties.Resources.hangman05;
+                        break;
+                    }
+
+                case 4:
+                    {
+                        pictureBox1.Image = Properties.Resources.hangman06;
+                        break;
+                    }
+
+                case 5:
+                    {
+                        pictureBox1.Image = Properties.Resources.hangman07;
+                        break;
+                    }
+
+                case 6:
+                    {
+                        pictureBox1.Image = Properties.Resources.dead;
+                        break;
+                    }
+            }
+
+            if (counter == 6 && userInputUsable != hangManAnswer)
+            {
+                label1.Text = "You've run out of tries!";
+                groupBox1.Visible = true;
+            }  
+     
+            else if (userInputUsable == hangManAnswer)
+            {
+                pictureBox1.Image = Properties.Resources.yay;
+                label1.Text = "Great! You got it!";
+                groupBox1.Visible = true;
+            }
+
+            else if (userInputUsable > hangManAnswer)
+            {
+            label1.Text = "The number is less than " + userInput;
+            }
+
+            else if (userInputUsable < hangManAnswer)
+            {
+            label1.Text = "The number is greater than " + userInput;
+            }
+            
+        }
+
+        private void radioButton1_CheckedChanged(object sender, EventArgs e)
+        {
+            hangManAnswer = rnd.Next(1, 100);
+            pictureBox1.Image = null;
+            counter = 0;
+            groupBox1.Visible = false;
+            radioButton1.Checked = false;
+            label1.Text = "Enter a number 1-100";
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            label1.Text = ("\tThe answer is: " + hangManAnswer);
         }
     }
 }
--- Homework4/Properties/Resources.Designer.cs
+++ Homework4/Properties/Resources.Designer.cs
@@ -8,10 +8,10 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace Homework4.Properties
-{
-
-
+namespace Homework4.Properties {
+    using System;
+    
+    
     /// <summary>
     ///   A strongly-typed resource class, for looking up localized strings, etc.
     /// </summary>
@@ -22,49 +22,121 @@ namespace Homework4.Properties
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "4.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources
-    {
-
+    internal class Resources {
+        
         private static global::System.Resources.ResourceManager resourceMan;
-
+        
         private static global::System.Globalization.CultureInfo resourceCulture;
-
+        
         [global::System.Diagnostics.CodeAnalysis.SuppressMessageAttribute("Microsoft.Performance", "CA1811:AvoidUncalledPrivateCode")]
-        internal Resources()
-        {
+        internal Resources() {
         }
-
+        
         /// <summary>
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager
-        {
-            get
-            {
-                if ((resourceMan == null))
-                {
+        internal static global::System.Resources.ResourceManager ResourceManager {
+            get {
+                if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("Homework4.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
             }
         }
-
+        
         /// <summary>
         ///   Overrides the current thread's CurrentUICulture property for all
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture
-        {
-            get
-            {
+        internal static global::System.Globalization.CultureInfo Culture {
+            get {
                 return resourceCulture;
             }
-            set
-            {
+            set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap dead {
+            get {
+                object obj = ResourceManager.GetObject("dead", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Hangman_Game_red {
+            get {
+                object obj = ResourceManager.GetObject("Hangman-Game-red", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap hangman01 {
+            get {
+                object obj = ResourceManager.GetObject("hangman01", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap hangman02 {
+            get {
+                object obj = ResourceManager.GetObject("hangman02", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap hangman05 {
+            get {
+                object obj = ResourceManager.GetObject("hangman05", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap hangman06 {
+            get {
+                object obj = ResourceManager.GetObject("hangman06", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap hangman07 {
+            get {
+                object obj = ResourceManager.GetObject("hangman07", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap yay {
+            get {
+                object obj = ResourceManager.GetObject("yay", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
             }
         }
     }
